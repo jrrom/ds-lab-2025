b0c7004cef5bbf843d677d0c4671a4c67fb80ea4
jrrom <77691121+jrrom@users.noreply.github.com>
2025-07-21 09:31:02 +0530

    Create Smallest, Largest.c

---
+++ Week 1/Smallest, Largest.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+// Week One - Smallest, Largest in an Array
+
+int size;
+int main() {
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    if (size < 1) {
+        printf("Size of the array must be greater than 1");
+        return -1;
+    }
+
+    int arr[size];
+    for (int i = 0; i < size; i++) {
+        printf("Enter number %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    int smallest = arr[0];
+    int largest  = arr[0];
+    for (int i = 0; i < size; i++) {
+        if (arr[i] < smallest) {
+            smallest = arr[i];
+            break;
+        }
+
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+
+    printf("\n\n");
+
+    printf("The largest number is %d\n", largest);
+    printf("The smallest number is %d", smallest);
+}
+
