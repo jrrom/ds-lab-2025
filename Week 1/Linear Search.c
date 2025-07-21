926c8181dfda9ecfb85a47f0f5ded98d044ba5f9
jrrom <77691121+jrrom@users.noreply.github.com>
2025-07-21 09:36:59 +0530

    Create Linear Search.c

---
+++ Week 1/Linear Search.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+# Week 1 - Linear Search
+
+int main() {
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    if (size < 1) {
+        printf("Size of the array must be greater than 1");
+        return -1;
+    }
+
+    int arr[size];
+    for (int i = 0; i < size; i++) {
+        printf("Enter number %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    int element;
+    printf("\nEnter the element to search for: ");
+    scanf("%d", &element);
+
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == element) {
+            printf("The element is at position %d in the array", i);
+        }
+    }
+}
